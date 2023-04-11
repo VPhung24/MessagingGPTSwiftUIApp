b2fe15156d9fe6a34128def7af0396d788f405aa
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-04-11 16:29:27 -0400

    Initial Commit

---
+++ Messaging/MessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingApp.swift
+//  Messaging
+//
+//  Created by Vivian Phung on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
